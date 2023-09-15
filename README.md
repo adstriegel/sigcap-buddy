a222a163f7d107a897b0c66e13661f205357c062
Al-Amin-ND <amohamm2@nd.edu>
2023-09-14 21:07:40 -0400

    Automatic ookla cli download and description of collcted parameters added

--- README.md
+++ README.md
@@ -3,10 +3,21 @@
 A tool to automatically log speedtest results on a Raspberry Pi.
 
 ## **Setup**
-**Prerequisites**:
 
+**Prerequisites**:
 - A Raspberry Pi with a working network connection.
 - Python 3 installed.
+- Ookla Speedtest CLI (Automatically downloaded and set up by the script if not present.)
+- `inotify-tools` package installed. 
+
+**Configuration**
+
+The program reads a config.json file to determine the interval between speed tests. The format is:
+
+{
+    "speedtest_interval": 5  // The interval, in minutes, between tests.
+}
+
 
 ## **Execution**
 
@@ -23,5 +34,33 @@ sudo systemctl start speedtest_logger.service
 
 4. To ensure the serve starts authomatically on boot: 
 sudo systemctl enable speedtest_logger.service
+
+
+## **Code Monitoring and Auto-Updates**
+
+The script `monitor_changes.sh` can be set up to monitor the codebase for any changes and auto-update the service upon any changes.
+
+1. Ensure the `monitor_changes.sh` script is executable:
+
+chmod +x monitor_changes.sh
+
+
+2. Run the script as a cronjob so it always starts at boot time
+
+## **Log Format**
+
+The program logs the results of each test in speedtest_log.json. Each entry is a separate line and is structured as:
+
+{
+    "start_time": "ISO timestamp when test started",
+    "end_time": "ISO timestamp when test ended",
+    "server": "Name of the test server",
+    "isp": "Name of your ISP",
+    "idle_latency": "Ping latency in ms",
+    "download_speed": "Download speed in bps",
+    "upload_speed": "Upload speed in bps",
+    "download_data_used": "Data used for the download test in bytes",
+    "upload_data_used": "Data used for the upload test in bytes"
+}
 
 
--- speedtest_logger.py
+++ speedtest_logger.py
@@ -3,8 +3,37 @@ import json
 import datetime
 import time
 import logging
+import os
 
 logging.basicConfig(filename='/home/netscale/Desktop/sigcap-buddy/speedtest_logger.log', level=logging.DEBUG)
+
+def download_speedtest():
+    """Download and extract the speedtest executable if not present."""
+    DOWNLOAD_ATTEMPTS = 3
+    SPEEDTEST_LINK = 'https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz'
+    
+    if not os.path.exists('./speedtest'):
+        for _ in range(DOWNLOAD_ATTEMPTS):
+            try:
+                # Download the tar file
+                subprocess.check_output(['wget', SPEEDTEST_LINK])
+                # Extract the tar file
+                subprocess.check_output(['tar', '-xvf', 'ookla-speedtest-1.2.0-linux-x86_64.tgz'])
+                # Check if speedtest exists in the current directory after extraction
+                if os.path.exists('speedtest/ookla-speedtest-1.2.0-linux-x86_64/speedtest'):
+                    # Copy the executable to the current directory
+                    subprocess.check_output(['cp', 'speedtest/ookla-speedtest-1.2.0-linux-x86_64/speedtest', '.'])
+                # Cleanup
+                #subprocess.check_output(['rm', '-r', 'speedtest'])
+                subprocess.check_output(['rm', 'ookla-speedtest-1.2.0-linux-x86_64.tgz'])
+                break
+            except subprocess.CalledProcessError:
+                logging.error(f"Attempt {_ + 1} to download speedtest failed.")
+        else:
+            logging.error("All attempts to download speedtest failed. Please ensure you have an active internet connection.")
+            logging.error(f"If the issue persists, locate the latest link for Ookla Speedtest CLI and update the link in {__file__}.")
+            raise SystemExit("Exiting due to failed speedtest download.")
+
 
 def read_config():
     with open("config.json", "r") as config_file:
@@ -40,6 +69,7 @@ def run_speedtest():
         log_file.write(json.dumps(log) + "\n")
 
 def main():
+    download_speedtest()  # Ensure speedtest executable is present
     while True:
         run_speedtest()
         interval = read_config()
