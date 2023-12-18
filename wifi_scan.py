a782cd01513910666485f6805bcdc466fcbe7715
Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
2023-12-18 02:17:56 -0600

    Added HT operation element decoding.

--- wifi_scan.py
+++ wifi_scan.py
@@ -50,6 +50,42 @@ def read_beacon_ie(ie_hex_string):
             output["type"] = "TPC Report"
             output["elements"]["tx_power"] = byte_uint_to_int(byte_data[2])
             output["elements"]["link_margin"] = byte_uint_to_int(byte_data[3])
+        case 61:
+            # HT Operation
+            output["type"] = "HT Operation"
+            output["elements"]["primary_channel"] = byte_data[2]
+
+            ht_operation_info = byte_data[3:8]
+            output["elements"]["secondary_channel_offset"] = (
+                ht_operation_info[0] & 0x03)
+            output["elements"]["sta_channel_width"] = (
+                (ht_operation_info[0] >> 2) & 0x01)
+            output["elements"]["rifs_mode"] = (
+                (ht_operation_info[0] >> 3) & 0x01)
+            output["elements"]["ht_protection"] = (
+                ht_operation_info[1] & 0x02)
+            output["elements"]["nongf_ht_sta_present"] = (
+                (ht_operation_info[1] >> 2) & 0x01)
+            output["elements"]["obss_nonht_sta_present"] = (
+                (ht_operation_info[1] >> 4) & 0x01)
+            output["elements"]["channel_center_freq_segment_2"] = (
+                ((ht_operation_info[1] >> 5) & 0x03)
+                + (ht_operation_info[2] << 8))
+            output["elements"]["dual_beacon"] = (
+                (ht_operation_info[3] >> 6) & 0x01)
+            output["elements"]["dual_cts_protection"] = (
+                (ht_operation_info[3] >> 7) & 0x01)
+            output["elements"]["stbc_beacon"] = (
+                ht_operation_info[4] & 0x01)
+            output["elements"]["lsig_txop_protection"] = (
+                (ht_operation_info[4] >> 1) & 0x01)
+            output["elements"]["pco_active"] = (
+                (ht_operation_info[4] >> 2) & 0x01)
+            output["elements"]["pco_phase"] = (
+                (ht_operation_info[4] >> 3) & 0x01)
+
+            output["elements"]["basic_mcs_set"] = int.from_bytes(
+                byte_data[8:24], byteorder='little')
         case _:
             output["type"] = "Unknown"
 
